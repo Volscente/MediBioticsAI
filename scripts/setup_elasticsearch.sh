7c7e65e3b8400822d4006f7807f1bc4ccdd5fe20
Simone Porreca <porrecasimone@gmail.com>
2023-11-12 19:08:46 +0100

    Update setup_elasticsearch.sh

---
+++ scripts/setup_elasticsearch.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# TODO: Script docstring
+# TODO: verify to run the script from the correct folder (https://learn.openwaterfoundation.org/owf-learn-linux-shell/best-practices/best-practices/)
+# Use the .env file to save the name of the root dir
+pwd=`pwd`
+echo `basename ${pwd}`
