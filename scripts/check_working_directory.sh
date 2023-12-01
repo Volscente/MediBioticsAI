920b3af85d06911c2ad8b0d77eb9cc0c48622578
Simone Porreca <porrecasimone@gmail.com>
2023-12-01 19:40:22 +0100

    Add path option and check working dir

--- scripts/check_working_directory.sh
+++ scripts/check_working_directory.sh
@@ -2,32 +2,52 @@
 # Verify whenever you are working from the corrected passed directory
 
 # Help Function
-Help()
-{
-  echo 'Verify whenever you are working from the corrected passed directory'
+show_help() {
+  echo "Usage: $0 [options]"
+  echo "Options:"
+  echo "  -h, --help         Display this help message."
+  echo "  -p, --path PATH    Specify the path for the script. (Required)"
 }
 
-# Switch Options
-while getopts ":h" option; do
-  case $option in
-    h) # Display Help
-      Help
-      exit;;
-    \?) # Incorrect Option
-      echo "Error: Invalid option"
-      exit;;
+# Parse command-line options
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -h|--help)
+      show_help
+      exit 0
+      ;;
+    -p|--path)
+      shift
+      path="$1"
+      ;;
+    *)
+      echo "Unknown option: $1."
+      show_help
+      exit 1
+      ;;
   esac
+  shift
 done
+
+echo "Checking the provided path."
+
+if [ -z "$path" ]; then
+  echo "Error: Please provide a path using the -p or --path option."
+  show_help
+  exit 1
+fi
 
 echo "Checking working directory..."
 
 # Retrieve current working directory
-directory_name=`basename $(pwd)`
+directory_name=$(basename "$(pwd)")
 
-echo "Working in the directory: ${directory_name}"
+echo "Working in the directory: ${directory_name}."
 
-#if [ ! ${dirname} = "build-util" ]
-#        then
-#        echo "Must run from build-util folder"
-#        exit 1
-#fi
+if [ ! "$directory_name" = "$path" ]; then
+  echo "Error: Must run from '$path' folder."
+  exit 1
+else
+  echo "The working directory is correct."
+  exit 0
+fi
