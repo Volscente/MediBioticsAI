ad7d07306bd266d6c254ef9379e09b8131ed129d
Simone Porreca <porrecasimone@gmail.com>
2023-12-22 20:53:15 +0100

    Add todos

---
+++ scripts/create_elasticsearch_user.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Create a new Elasticsearch user
+# Reference guide: https://www.elastic.co/guide/en/elasticsearch/reference/current/users-command.html
+
+# Verify that the script runs from the correct folder
+./scripts/check_working_directory.sh --path "$ROOT_DIRECTORY"
+
+# TODO
+# Check if Elasticsearch container is running
+
+# TODO
+# Check if Elasticsearch is reachable
+
+# TODO
+# Create Elasticsearch user
