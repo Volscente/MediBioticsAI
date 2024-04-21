556afb47eb95608ed93f496f4aee863a7ad25843
Simone Porreca <porrecasimone@gmail.com>
2024-04-21 20:34:43 +0200

    Add build_categorical_data_pipeline_steps

--- src/data_preparation/data_preparation_utils.py
+++ src/data_preparation/data_preparation_utils.py
@@ -78,3 +78,65 @@ def build_numerical_data_pipeline_steps(numerical_data_transformations: dict) ->
 
     return numerical_data_pipeline_steps
 
+
+def build_categorical_data_pipeline_steps(categorical_data_transformations: dict) -> list:
+    """
+    Build categorical data transformations steps based
+    on the configuration in 'categorical_data_transformations'
+
+    Args:
+        categorical_data_transformations: Dictionary of categorical
+                                          data transformations configuration
+
+    Returns:
+        categorical_data_pipeline_steps: List of categorical data transformation steps
+    """
+    logger.info('build_categorical_data_pipeline_steps - Start')
+
+    # Initialise categorical data pipeline steps list
+    categorical_data_pipeline_steps = []
+
+    logger.info('build_categorical_data_pipeline_steps - Building steps')
+
+    # 1. Check imputation step
+    if categorical_data_transformations['imputation']['include']:
+
+        # Retrieve imputation module to use
+        imputation_module = categorical_data_transformations['imputation']['module']
+
+        logger.info('build_categorical_data_pipeline_steps - Adding %s Imputation step',
+                    imputation_module)
+
+        # Switch the imputation technique to apply
+        match imputation_module:
+            case 'SimpleImputer':
+                categorical_data_pipeline_steps.append(
+                    ('imputation',
+                     SimpleImputer(strategy='constant', fill_value='unknown', copy=False))
+                )
+    else:
+        logger.info('build_categorical_data_pipeline_steps - Skipping Imputation step')
+
+    # 2. Check one hot encoding step
+    if categorical_data_transformations['one_hot_encoding']['include']:
+
+        # Retrieve one hot encoding module to use
+        one_hot_encoding_module = categorical_data_transformations['one_hot_encoding']['module']
+
+        logger.info('build_categorical_data_pipeline_steps - Adding %s One-hot Encoding step',
+                    one_hot_encoding_module)
+
+        # Switch the one hot encoding technique to apply
+        match one_hot_encoding_module:
+            case 'OneHotEncoder':
+                categorical_data_pipeline_steps.append(
+                    ('one_hot_encoding',
+                     OneHotEncoder())
+                )
+    else:
+        logger.info('build_categorical_data_pipeline_steps - Skipping One-hot Encoding step')
+
+    logger.info('build_categorical_data_pipeline_steps - End')
+
+    return categorical_data_pipeline_steps
+
