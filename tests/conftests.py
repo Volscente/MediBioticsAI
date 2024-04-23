44be0ceccd768f5b0b531df3cced959d4841cab1
Simone Porreca <porrecasimone@gmail.com>
2024-04-23 13:34:03 +0200

    Add fixture_numerical_data_transformations

--- tests/conftests.py
+++ tests/conftests.py
@@ -13,3 +13,23 @@ from src.general_utils.general_utils import read_configuration
 configuration = read_configuration(pathlib.Path(__file__).parents[1]
                                    / 'configuration'
                                    / 'test_config.yaml')
+
+
+@pytest.fixture
+def fixture_numerical_data_transformations(
+        test_numerical_data_transformations: dict = configuration['test_numerical_data_transformations']
+) -> dict:
+    """
+    Fixture for a Dictionary Numerical Data Transformations with structure:
+        <transformation_name>:
+            include: <bool>
+            module: <string module name>
+
+    Args:
+        test_numerical_data_transformations: dict of numerical data transformations
+
+    Returns:
+        test_numerical_data_transformations: dict of numerical data transformations
+    """
+
+    return test_numerical_data_transformations
